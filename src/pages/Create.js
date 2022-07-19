ee5ce0ac625a63520d841b936f458b096be44a2e
Bruno Peres <bruno_16515@hotmail.com>
2022-07-19 14:39:37 -0300

    Typography

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import { Typography } from "@material-ui/core";
+//import Typography from "@material-ui/core/Typography";
 
 export default function Create() {
   return (
     <div>
-      Create page
+      <Typography variant="h6" component="h2" color="textSecondary" gutterBottom>
+        Create a New Note
+      </Typography>
     </div>
-  )
+  ); 
 }

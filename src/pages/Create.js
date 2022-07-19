c30cd9ea5f49304ded3972fdaf8766e0a1ded24d
Bruno Peres <bruno_16515@hotmail.com>
2022-07-19 14:50:30 -0300

    Buttons

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,13 +1,36 @@
 import React from "react";
-import { Typography } from "@material-ui/core";
-//import Typography from "@material-ui/core/Typography";
+import { Typography, Button, Container } from "@material-ui/core";
 
 export default function Create() {
   return (
-    <div>
-      <Typography variant="h6" component="h2" color="textSecondary" gutterBottom>
+    <Container>
+      <Typography
+        variant="h6"
+        component="h2"
+        color="textSecondary"
+        gutterBottom
+      >
         Create a New Note
       </Typography>
-    </div>
-  ); 
+
+      <Button
+        onClick={() => console.log("You clicked me")}
+        type="submit"
+        color="secondary"
+        variant="contained"
+      >
+        Submit
+      </Button>
+
+      {/* <Button type="submit">Submit</Button>
+      <Button type="submit" color="secondary" variant="outlined">Submit</Button>
+
+      <ButtonGroup color="primary" variant="contained">
+        <Button>One</Button>
+        <Button>Two</Button>
+        <Button>Three</Button>
+        <Button>Four</Button>
+      </ButtonGroup> */}
+    </Container>
+  );
 }

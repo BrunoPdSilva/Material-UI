ff1d016a3786f4b92f094839bccbcada11f020ce
Bruno Peres <bruno_16515@hotmail.com>
2022-07-19 15:56:54 -0300

    CustomThemes att

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Notes from './pages/Notes'
-import Create from './pages/Create'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@material-ui/core/styles";
+import { green } from "@material-ui/core/colors";
+
+import Notes from "./pages/Notes";
+import Create from "./pages/Create";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#e8f5e9"
+    },
+  },
+});
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Notes />
-        </Route>
-        <Route path="/create">
-          <Create />
-        </Route>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Notes />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- src/pages/Create.js
+++ src/pages/Create.js
@@ -2,19 +2,7 @@ import React from "react";
 import { Typography, Button, Container, makeStyles } from "@material-ui/core";
 import { KeyboardArrowRight } from "@material-ui/icons";
 
-const useStyles = makeStyles({
-  btn: {
-    fontSize: 60,
-    backgroundColor: "violet",
-    "&:hover": {
-      backgroundColor: "blue",
-    },
-  },
-  title: {
-    textDecoration: "underline",
-    marginBottom: 20,
-  },
-});
+const useStyles = makeStyles({});
 
 export default function Create() {
   const classes = useStyles();
@@ -22,7 +10,6 @@ export default function Create() {
   return (
     <Container>
       <Typography
-        className={classes.title}
         variant="h6"
         component="h2"
         color="textSecondary"
@@ -32,10 +19,9 @@ export default function Create() {
       </Typography>
 
       <Button
-        className={classes.btn}
         onClick={() => console.log("You clicked me")}
         type="submit"
-        color="secondary"
+        color="primary"
         variant="contained"
         endIcon={<KeyboardArrowRight />}
       >

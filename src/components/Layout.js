653ff4824aa32c66b4cc7737a99b884fdf5ca1e6
Bruno Peres <bruno_16515@hotmail.com>
2022-07-22 16:52:40 -0300

    Drawer finalizado

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,20 +1,41 @@
-import { makeStyles } from "@material-ui/core";
+import { makeStyles, Drawer, Typography } from "@material-ui/core";
+
+const drawerWidth = 240;
 
 const useStyles = makeStyles({
   page: {
-    backgroundColor: '#f9f9f9',
-    width: '100%',
+    backgroundColor: "#f9f9f9",
+    width: "100%",
+  },
+  drawer: {
+    width: drawerWidth,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  root: {
+    display: "flex"
   }
-})
+});
 
 export function Layout({ children }) {
   const classes = useStyles();
 
   return (
-    <div>
+    <div className={classes.root}>
       {/* app bar */}
 
       {/* side drawer */}
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        anchor="left"
+        classes={{ paper: classes.drawerPaper }}
+      >
+        <div>
+          <Typography variant="h5">Ninja Notes</Typography>
+        </div>
+      </Drawer>
 
       <div className={classes.page}>{children}</div>
     </div>

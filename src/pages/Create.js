e17337d2149160a4c6a41f8b7586e7d01167200d
Bruno Peres <bruno_16515@hotmail.com>
2022-07-22 09:35:42 -0300

    Lesson-07 Text Fields concluída

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,11 +1,47 @@
-import React from "react";
-import { Typography, Button, Container, makeStyles } from "@material-ui/core";
+import React, { useState } from "react";
+import {
+  Typography,
+  Button,
+  Container,
+  makeStyles,
+  TextField,
+} from "@material-ui/core";
 import { KeyboardArrowRight } from "@material-ui/icons";
 
-const useStyles = makeStyles({});
+const useStyles = makeStyles({
+  field: {
+    marginTop: 20,
+    marginBottom: 20,
+    display: "block",
+  },
+});
 
 export default function Create() {
+  const [title, setTitle] = useState("");
+  const [details, setDetails] = useState("");
+  const [titleError, setTitleError] = useState(false);
+  const [detailsError, setDetailsError] = useState(false);
+
   const classes = useStyles();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    setTitleError(false);
+    setDetailsError(false);
+
+    if (title == '') {
+      setTitleError(true)
+    }
+
+    if (details == '') {
+      setDetailsError(true)
+    }
+
+    if (title && details) {
+      console.log(title, details);
+    }
+  };
 
   return (
     <Container>
@@ -17,16 +53,37 @@ export default function Create() {
       >
         Create a New Note
       </Typography>
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <TextField
+          onChange={e => setTitle(e.target.value)}
+          className={classes.field}
+          label="Note Title"
+          variant="outlined"
+          fullWidth
+          required
+          error={titleError}
+        />
 
-      <Button
-        onClick={() => console.log("You clicked me")}
-        type="submit"
-        color="primary"
-        variant="contained"
-        endIcon={<KeyboardArrowRight />}
-      >
-        Submit
-      </Button>
+        <TextField
+          onChange={e => setDetails(e.target.value)}
+          className={classes.field}
+          label="Details"
+          variant="outlined"
+          multiline
+          minRows={4}
+          fullWidth
+          required
+          error={detailsError}
+        />
+        <Button
+          type="submit"
+          color="primary"
+          variant="contained"
+          endIcon={<KeyboardArrowRight />}
+        >
+          Submit
+        </Button>
+      </form>
     </Container>
   );
 }
